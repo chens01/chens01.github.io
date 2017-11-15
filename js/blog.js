b2979aec916490b153a2ff483e8b55c913418af6
chens01 <chens01@mysoft.com.cn>
2017-11-15 16:00:46 +0800

    修改

--- js/blog.js
+++ js/blog.js
@@ -12,7 +12,6 @@ $(function() {
 
 // make all images responsive
 /* 
- * Unuse by Hux
  * actually only Portfolio-Pages can't use it and only post-img need it.
  * so I modify the _layout/post and CSS to make post-img responsive!
  */
